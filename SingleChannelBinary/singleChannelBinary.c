c38688000c2b4fdfdb7f8fe6cd224a23bca217fa
Lubos Vozdecky <vozdeckyl@gmail.com>
2023-02-15 20:35:22 +0000

    added program for reading binary input (single channel)

---
+++ SingleChannelBinary/singleChannelBinary.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "pico/lock_core.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
+
+int counter = 0;
+static mutex_t mutex;
+
+void send_data()
+{
+    int data = 0;
+    uint64_t timeOfLastReading;
+
+    while(true) {
+
+	char input;
+	// wait until \r (return) is sent
+	while((input = getchar()) != 0x0D)
+	{
+	    sleep_ms(1);
+	}
+	
+	mutex_enter_blocking(&mutex);
+	data = counter;
+	counter = 0;
+	mutex_exit(&mutex);
+	int measurementDuration_us = to_us_since_boot(get_absolute_time()) - timeOfLastReading; 
+	printf("%d %d %f \n", data, measurementDuration_us, 1e6*((float) data)/((float) measurementDuration_us));
+	timeOfLastReading = to_us_since_boot(get_absolute_time());
+    }
+}
+
+int main() {
+    stdio_init_all();
+    
+    uint inputPin = 16;
+    gpio_init(inputPin);
+    gpio_set_input_enabled(inputPin, true);
+    
+    mutex_init(&mutex);
+    multicore_launch_core1(send_data);
+    
+    uint16_t signal;
+    
+    
+    while (true) {
+	
+	if(gpio_get(inputPin)) {
+	    mutex_enter_blocking(&mutex);
+	    counter++;
+	    mutex_exit(&mutex);
+	    
+	    while(gpio_get(inputPin)) {
+		// wait until the signal comes back down
+	    }
+	}
+    }
+    return 0;
+}
